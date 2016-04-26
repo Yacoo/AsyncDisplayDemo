6ac2424611ae3196ccf1743dec56a7e43065f77e
name <yake.wang@liwushuo.com>
2016-04-26 22:44:11 +0800

    first commit

---
+++ AsyncDisplayKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AsyncDisplayKit
+//
+//  Created by yake on 16/4/26.
+//  Copyright © 2016年 XF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
